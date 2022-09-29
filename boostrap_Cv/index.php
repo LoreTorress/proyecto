d1aebf0e03415a6f7ba80f25a1f5edd54d380f93
LoreTorress <114442520+LoreTorress@users.noreply.github.com>
2022-09-30 01:09:49 +0200

    Add files via upload

---
+++ boostrap_Cv/index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/a2d042081a.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style_CV.css">
+ 
+</head>
+<style>
+body {
+  background-image: url('img/fondo.webp');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<body>
+<!-- Navbar content -->
+
+<span class="border border-primary">
+	<div class="container-fluid">
+
+		<nav class="navbar navbar-light">
+    <a class="navbar-brand" href="#">Curriculum Vitae</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="# Sobre mi">Sobre mi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Estudios">Estudios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Portafolio">Portafolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Intereses">Intereses</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#Contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--------1Parte----------------------------------------------------------->
+<div id="Sobre mi" class="container">
+			<div class="row" align="center">
+				<div class="col-6">
+					<h3>Lorena Torres Cuazitl</h3>
+					<img src="img/foto1.jpeg" alt="Lorena" width="50%">
+				</div>
+				<div class="col-6" align="center">
+					<br><br><br><br>
+					<p>No te pliegues. No lo diluyas. No intentes hacerlo lógico. No adaptes tu propia alma a las costumbres de los demás. En lugar de todo eso, sigue tu propia obsesión implacablemente
+					</p>
+				</div>
+			</div>
+			</div>
+<!----------Estudios_Acordeon--------------------------------------->
+<br><br><br>
+<div id="Estudios" class="container-fluid">
+			<div class="row">
+				<div class="col-12">
+				<h1 align="center">Estudios</h1>
+				</div>
+			</div>
+<div class="row">
+				<div class="col-8">
+			<div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Secundaria
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body"><code>Secundaria Tecnica #19 Vicente Suarez Ferrer </code> Estudie de 2014-2017</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Bachillerato
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body"><code>General Oficial Josefa Ortiz de Dominguez</code> Estudie de 2017-2020-</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        TSU
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body"><code>Tecnologico de huejotzingo</code> Lo estoy cursando entre en el año 2021</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+        Cursos
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body"><code>Ofimatica</code> Los tome en el año 2020 Obtube la certificacion</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+        Conocimientos y habilidades
+      </button>
+    </h2>
+    <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body"><code>Office</code> Conocimientos basicos en la paqueteria de office</div>
+    </div>
+  </div>
+</div>	
+</div>
+<!----------Carrousel-------------------------------------------------------->
+<div class="col-4" >
+	<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/f1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Secundaria</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Bachiller</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>TSU</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Cursos</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f5.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Conocimientos y habilidades</h5>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div></div><br><br><br>
+<!------------Portafolio------------------------------------------>
+<div id="Portafolio" class="container overflow-hidden">
+  <div class="row gx-5">
+  	<h1 align="center">Portafolio</h1>
+    <div class="col">
+     <div class="p-3 border bg-light">
+     		<img src="img/f6.jpeg" alt="Primer" width="15%">
+					<h6>Primer Cuatrimestre</h6>
+					<p align="right">Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Tempore, mollitia, odio. Atque temporibus, ullam qui, adipisci totam dicta numquam ipsam officiis doloremque, id perferendis similique reprehenderit praesentium dignissimos ex commodi!
+					</p>
+				</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border bg-light">
+      	<img src="img/f7.jpeg" alt="Primer" width="15%">
+					<h6>Segundo Cuatrimestre</h6>
+					<p>Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Tempore, mollitia, odio. Atque temporibus, ullam qui, adipisci totam dicta numquam ipsam officiis doloremque, id perferendis similique reprehenderit praesentium dignissimos ex commodi!
+					</p>
+				</div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<!------------Portafolio-Carrousel-----------------------------------------> 
+<div class="container">
+				<div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/f8.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/f9.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/f10.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  <div class="col-2"></div>
+  </div></div></div></div></div>
+<!------------Intererses------------------------------------------>
+<div id="Intereses" class="container">
+
+<div class="container px-4">
+  <div class="row gx-5">
+  	<h1 align="center">Intereses</h1>
+    <div class="col">
+
+     <div class="p-3 border bg-light" align="center"><i class="fa-solid fa-book-open-reader" ></i><br><h6>Lectura</h6>
+					<p>Driario de Ana Frank</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border bg-light" align="center"><i class="fa-solid fa-ranking-star"></i><br><h6>Deportes</h6>
+					<p>No practico deporte</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border bg-light" align="center"><i class="fa-solid fa-book-open-reader"></i><br><h6>Lectura</h6>
+					<p>No leo mucho libros</div>
+    </div>
+  </div>
+</div>
+</div>
+<!------------Contacto------------------------------------------>
+	<div id="Contacto" class="container-fluid">
+		<div class="container">
+			<h1 align="center">Contacto</h1>
+			<div class="row">
+				<div class="col-4">
+					<h6>Redes Sociales</h6>
+					<p><i class="fa-brands fa-facebook-square"> Lore de Perez</i><br>
+					<i class="fa-brands fa-telegram"> Lore Torres</i><br>
+					<i class="fa-brands fa-twitter-square"> Lore Torres</i><br>
+					</p>
+				</div>
+				<div class="col-8">
+					<p><i class="fa-solid fa-map-location"><br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.576787193841!2d-98.27424938509876!3d19.082336287084274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4985573b0b8c261!2zMTnCsDA0JzU2LjQiTiA5OMKwMTYnMTkuNCJX!5e0!3m2!1ses!2smx!4v1657670647248!5m2!1ses!2smx" width="150%" height="400%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></i><br>
+						<i class="fa-solid fa-mobile-screen"> 5567421683</i><br>
+						<i class="fa-solid fa-message"> 5567421683</i>
+
+					</p>
+				</div>
+			</div>
+	</div>
+<!------------Derechos------------------------------------------>
+<div class="container">
+
+<div class="container px-4">
+  <div class="row gx-5">
+  	<h3 align="center">Derechos Reservados 2022</h3>
+  </div>
+</div>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
